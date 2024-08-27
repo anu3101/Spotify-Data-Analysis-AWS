01c331b7c7a8d82830cafcda0d748eb6281bf47c
Anudesh Nigam <95866010+anu3101@users.noreply.github.com>
2024-08-27 21:21:04 +0530

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -2,9 +2,7 @@ SPOTIFY_DATASPOTIFY_DATA.PUBLICUSE ROLE ACCOUNTADMIN;
 
 USE WAREHOUSE COMPUTE_WH;
 
-SELECT * FROM SPOTIFY_DATA.PUBLIC.SPOTIFY;
-
-create or replace TABLE SPOTIFY_DATA.PUBLIC.SPOTIFY (
+CREATE TABLE SPOTIFY_DATA.PUBLIC.SPOTIFY (
 	ARTIST_ID VARCHAR(500),
 	ARTIST_NAME VARCHAR(500),
 	ARTIST_POPULARITY NUMBER(38,0),
@@ -22,56 +20,108 @@ create or replace TABLE SPOTIFY_DATA.PUBLIC.SPOTIFY (
 	TRACK_POPULARITY NUMBER(38,0)
 );
 
---Data Checks
+UPDATE spotify
+SET Genre='Unknown'
+WHERE genre ='Not_known';
+
 SELECT * FROM SPOTIFY 
 LIMIT 20;
 
---COUNT Songs and Genre
+--Count total tracks in data
+SELECT COUNT(track_id) AS total_tracks FROM spotify;
+
+--Count unique artists
+SELECT COUNT(DISTINCT artist_name) AS unique_artists 
+FROM spotify;
+
+--List all unique genres
+SELECT DISTINCT genre 
+FROM spotify
+WHERE genre IS NOT NULL;
+
+--Most 5 popular artists
+SELECT DISTINCT artist_name, followers 
+FROM spotify
+ORDER BY followers DESC 
+LIMIT 5;
+
+
+--Average duration in min
+SELECT ROUND(AVG(duration_sec/60),2) AS avg_duration_sec 
+FROM SPOTIFY;
+
+--Albums with more than 50 songs
+SELECT album_name, total_tracks 
+FROM spotify
+WHERE total_tracks > 50
+GROUP BY album_name, TOTAL_TRACKS
+ORDER BY total_tracks DESC;
+
+
+--Top 20 most popular songs and their artits
+SELECT track_name, track_popularity, artist_name
+FROM spotify
+ORDER BY track_popularity DESC
+LIMIT 20;
+
+--COUNT genre wise tracks
 SELECT genre, COUNT(track_id) AS track_counts
 FROM spotify
 GROUP BY genre
 ORDER BY track_counts DESC
 LIMIT 20;
 
---Top 20 most popular songs and their artits
 
-SELECT track_name, track_popularity, artist_name
-FROM spotify
-ORDER BY track_popularity DESC
-LIMIT 20;
-
---Track popularity by genre
+--Average Genre Popularity
 SELECT UPPER(genre), AVG(track_popularity) AS avg_track_popularity
 FROM spotify
 GROUP BY genre
 ORDER BY avg_track_popularity DESC
 LIMIT 20;
 
---TOP 10 Artists
-SELECT DISTINCT artist_name, followers
-FROM SPOTIFY
-ORDER BY followers DESC
-LIMIT 10;
 
---Artists Tracks Counts
+--Most songs by artists
 SELECT artist_name, COUNT(track_id) AS total_tracks
 FROM SPOTIFY
 GROUP BY artist_name
 ORDER BY total_tracks DESC
 LIMIT 20;
 
---Albums Popularity and Artists 
+--Most popular albums of artists
 SELECT artist_name, album_name, MAX(album_popularity) AS most_popular_album
 FROM SPOTIFY
 GROUP BY artist_name, album_name
 ORDER BY most_popular_album DESC
 LIMIT 20;
 
---Avg duration of songs in different genre
+-- Average Songs duration in differnet genre
 SELECT genre, AVG(duration_sec) AS avg_duration
 FROM spotify
 GROUP BY genre
 ORDER BY avg_duration DESC
 LIMIT 20;
 
+--Top 10 songs of Taylor Swift
+SELECT track_name, track_popularity 
+FROM spotify
+WHERE artist_name = 'Taylor Swift' 
+ORDER BY track_popularity DESC 
+LIMIT 10;
+
+
+SELECT genre, track_name, track_popularity
+FROM (
+    SELECT genre, track_name, track_popularity,
+           ROW_NUMBER() OVER (PARTITION BY genre ORDER BY track_popularity DESC) AS rank
+    FROM spotify
+) AS ranked_tracks
+WHERE rank <= 3;
+
+SELECT DISTINCT artist_name, track_name, release_date
+FROM spotify AS outer_table
+WHERE release_date = (
+    SELECT MAX(release_date)
+    FROM spotify AS inner_table
+    WHERE inner_table.artist_name = outer_table.artist_name
+);
 
